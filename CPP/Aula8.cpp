986b1e7d6f514a8a12368a6526f81614e2b564b6
Lucas de Souza Santos <lucas210souza@gmail.com>
2026-07-28 19:54:52 -0300

    Final da Aula 08

--- CPP/Aula8.cpp
+++ CPP/Aula8.cpp
@@ -4,5 +4,23 @@ using namespace std;
 
 int main()
 {
+    int num = 10;
+
+    cout << num << endl;
+
+    // INVERTER O VALOR DA VARIAVEL
+
+    num = num * -1;
+
+    cout << "Valor invertido: " << num << endl;
+
+    num = num * -1;
+
+    cout << "Valor invertido Mometaneo na memoria: " << -num << endl;
+
+    // FORMA DE GRAVAR NA MEMORIA RAM
+    num *= -1;
+    cout << "Valor invertido: " << num << endl;
+
     return 0;
 }
