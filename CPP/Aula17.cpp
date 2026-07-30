121f76dc2d6416cb5d5e0f18a9ee41bd3fe97d45
Lucas de Souza Santos <lucas210souza@gmail.com>
2026-07-30 19:56:33 -0300

    Final da aula 17 acrescentando novas maneiras de usar o laço for...

--- CPP/Aula17.cpp
+++ CPP/Aula17.cpp
@@ -4,9 +4,22 @@ using namespace std;
 
 int main()
 {
-    for(int i = 1; i <= 10; i++)
+    system("cls");
+
+    // for(int i = 1; i <= 10; i++)
+    // {
+    //     cout << i << endl;
+    // }
+
+    
+    // for(int x = 0, y = 0; x <= 10 || y <= 10; x++, y+=2)
+    // {
+    //     cout << x * y << endl;
+    // }
+
+    for(int x = 0, y = 1, z = 2; x <= 10 || y <= 10 || z <= 10; x++, y+=2, z+=2)
     {
-        cout << i << endl;
+        cout <<"Indice = " << x << " Impar = " << y << " Par = " << z << endl;
     }
 
     return 0;
